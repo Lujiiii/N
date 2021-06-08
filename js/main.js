ce4b634dc81fe62bfc49f80c958cc4088ad5c439
Lujiiii <85557673+Lujiiii@users.noreply.github.com>
2021-06-08 13:25:37 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var getusername = prompt("ujhbuggi");
+console.log(getusername);
+document.getElementById("username").innerText = getusername;
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
